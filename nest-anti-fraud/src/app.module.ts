eaff4a8e9e47d1177be4610a51abfb2183ec9cc5
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-04-14 17:04:14 -0300

    feat: :sparkles: Criando endpoints de consulta para invoices

--- nest-anti-fraud/src/app.module.ts
+++ nest-anti-fraud/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { InvoicesModule } from './invoices/invoices.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { InvoicesService } from './invoices/invoices.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        SUSPICIOUS_VARIATION_PERCENTAGE: Joi.number(),
+        INVOICES_HISTORY_COUNT: Joi.number(),
+        SUSPICIOUS_INVOICES_COUNT: Joi.number(),
+        SUSPICIOUS_TIMEFRAME_HOURS: Joi.number(),
+      }),
+    }),
+    InvoicesModule,
+    PrismaModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [InvoicesService],
 })
 export class AppModule {}
